6c2254f75cab7586c0dd8bb15855dedbb9032c62
Vadim210507 <143997107+Vadim210507@users.noreply.github.com>
2023-09-04 11:34:45 +0300

    первая задача Иванов Вадим

---
+++ Easy4.cpp
@@ -0,0 +1,6 @@
+#include "Easy6.h"
+#include <iostream>
+
+void PrintName() {
+    cout << "Vadim";
+}
---
+++ Easy6.h
@@ -0,0 +1,7 @@
+#ifndef EASY6_H_INCLUDED
+#define EASY6_H_INCLUDED
+using namespace std;
+
+void PrintName();
+
+#endif // EASY6_H_INCLUDED
